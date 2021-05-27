11fc37d292f0cd0ab2814c078371b0eedff0a562
zolamagama <zolamagama1985@gmail.com>
2021-05-27 22:03:37 +0200

    added overall counter and individual counter methods

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -6,23 +6,44 @@ public class Greet {
 
     public String greetName(String name, int insert) {
         String greetMessage = "";
-        switch (insert) {
-            case 1:
-                greetMessage = " ,مرحبا" + name;
-                greetedNames.add(name);
 
-                break;
-            case 2:
-                greetMessage = "Hello, " + name;
-                greetedNames.add(name);
+        if(!greetedNames.contains(name)) {
+            switch (insert) {
+                case 1:
+                    greetMessage = " ,مرحبا" + name;
+                    greetedNames.add(name);
 
-                break;
-            case 3:
-                greetMessage = "Olá, " + name;
-                greetedNames.add(name);
-                break;
+                    break;
+                case 2:
+                    greetMessage = "Hello, " + name;
+                    greetedNames.add(name);
+
+                    break;
+                case 3:
+                    greetMessage = "Olá, " + name;
+                    greetedNames.add(name);
+                    break;
+            }
         }
         return greetMessage;
+    }
+
+    public int counterForIdenticalName(String userName) {
+        
+        ArrayList<String> specificUserName = new ArrayList<>();
+
+        for (String name: greetedNames) {
+            if(name.equals(userName)) {
+                specificUserName.add(userName);
+            }
+        }
+    return specificUserName.size();
+    }
+
+    public int counter() {
+
+    return greetedNames.size();
+
     }
 
     public static void main(String[] args) {
